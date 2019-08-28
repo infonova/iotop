39006434b6b6f6940f24ad5b73c96dcb2b2a1de3
Primoz Kovacic <primoz.kovacic@bearingpoint.com>
2019-08-28 11:25:22 +0200

    Add config for grouping processes

--- iotop/ui.py
+++ iotop/ui.py
@@ -30,6 +30,7 @@ import signal
 import sys
 import time
 import re
+import yaml
 from prometheus_client import CollectorRegistry, Counter, write_to_textfile
 
 # Try to ensure time.monotonic() is available.
@@ -163,13 +164,29 @@ class IOTopUI(object):
         (lambda p, s: p.get_cmdline(), False),
     ]
 
+
+    def load_iotop_configuration():
+        if os.path.isfile("/etc/iotop-config.yml"):
+            return yaml.safe_load(open("etc/iotop-config.yml", "r"))
+        return None
+
+    iotop_configuration = load_iotop_configuration()
+
     def __init__(self, win, process_list, options):
 
         if options.prometheus:
             self.prom_metrics_prefix = '%s_%s' % (options.prometheus_namespace, options.prometheus_subsystem)
             self.prom_registry = CollectorRegistry()
-            self.prom_write_bytes_total = Counter('%s_writes_bytes_total' % self.prom_metrics_prefix, 'The total number of bytes written by a process or thread', ['pid', 'user', 'cmd'], registry=self.prom_registry)
-            self.prom_read_bytes_total = Counter('%s_reads_bytes_total' % self.prom_metrics_prefix, 'The total number of bytes read by a process or thread', ['pid', 'user', 'cmd'], registry=self.prom_registry)
+
+            if self.iotop_configuration == None:
+                pid_list = ['pid', 'user', 'cmd']
+                description = 'The total number of bytes {} by a process or thread'
+            else:
+                pid_list = ['process_group']
+                description = 'The total number of bytes {} by a process group'
+
+            self.prom_read_bytes_total = Counter('%s_reads_bytes_total' % self.prom_metrics_prefix, description.format('read'), pid_list, registry=self.prom_registry)
+            self.prom_write_bytes_total = Counter('%s_writes_bytes_total' % self.prom_metrics_prefix, description.format('written'), pid_list, registry=self.prom_registry)
 
         self.process_list = process_list
         self.options = options
@@ -445,6 +462,14 @@ class IOTopUI(object):
             return re.sub(r"([\n\"\\])", escape_character, value)
 
         def format(p):
+
+            def determine_process_group(cmdline):
+                for process_group in self.iotop_configuration["process_groups"]:
+                    if re.match(process_group["pattern"], cmdline):
+                      return process_group["name"]
+
+                return self.iotop_configuration["default_process_group"]
+
             stats = format_stats(self.options, p, self.process_list.duration)
             io_delay, swapin_delay, read_bytes, write_bytes = stats
 
@@ -454,16 +479,22 @@ class IOTopUI(object):
                 delay_stats = ' ?unavailable?  '
             pid_format = '%%%dd' % MAX_PID_WIDTH
             if self.options.prometheus:
-                escaped_cmdline = escape_value(p.get_cmdline()).strip()
 
                 read_bytes_float = float(read_bytes)
                 write_bytes_float = float(write_bytes)
 
+                if self.iotop_configuration == None:
+                    escaped_cmdline = escape_value(p.get_cmdline()).strip()
+                    labels = [p.pid, p.get_user()[:8], escaped_cmdline]
+                else:
+                    process_group = determine_process_group(p.get_cmdline())
+                    labels = [process_group]
+
                 if (read_bytes_float > 0.0):
-                    self.prom_read_bytes_total.labels(pid=p.pid,user=p.get_user()[:8],cmd=escaped_cmdline).inc(read_bytes_float)
+                    self.prom_read_bytes_total.labels(*labels).inc(read_bytes_float)
 
                 if (write_bytes_float > 0.0):
-                    self.prom_write_bytes_total.labels(pid=p.pid,user=p.get_user()[:8],cmd=escaped_cmdline).inc(write_bytes_float)
+                    self.prom_write_bytes_total.labels(*labels).inc(write_bytes_float)
 
                 line = ""
             else:
